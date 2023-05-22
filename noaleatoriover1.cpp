85839aebb912ea1731d0c06e6129fcd46d1065de
HilArabelle <132083357+HilArabelle@users.noreply.github.com>
2023-05-22 08:10:26 -0600

    Add files via upload

---
+++ noaleatoriover1.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main ()
+{
+	float num,num2;
+	num=rand( ) % 100;
+	printf ("Ingresa un numero");
+	scanf ("$f",num2);
+	printf ("%d",num)
+	if (x=num)
+	{
+		printf ("Numero correcto");
+	}
+	else
+	{
+		printf ("Numero incorrecto");
+	}
+	return 0; 
+}
---
+++ noaqleatoriover2.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+int main (int argc,char**argv)
+{
+	int num,num2,opc=0;
+	printf ("\n Adivinar numero");
+	printf ("\n1-Comenzar");
+	printf ("\n2-Salir.\n");
+	printf ("\n Introduce un opcion");
+	scanf ("%d",&opc);
+	while (opc!=2)
+	{
+		num=rand() % 100;
+		printf("\n introduce numero");
+		 scanf("%d",&num2);
+       
+        while(num!=num2)
+        {
+            if (num>num2)
+               printf("Es mayor");
+            else
+                 printf("Es menor");
+                
+            printf("\n Introduce numero: ");
+            scanf("%d",&num2);
+        }
+       
+        printf("\n Has acertado! \n");
+       
+        printf("\n 1 - Jugar de nuevo.");
+        printf("\n 2 - Salir.");
+        printf("\n Introduce una opcion:");
+       
+        scanf("%d",&opc);
+    } 
+    system("PAUSE"); 
+		
+	return 0;
+}
