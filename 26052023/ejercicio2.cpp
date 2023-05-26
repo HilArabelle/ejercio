cdf63a9564447b1cf471c8bdd0d44da258d79d25
HilArabelle <132083357+HilArabelle@users.noreply.github.com>
2023-05-26 10:52:46 -0600

    Add files via upload

---
+++ 26052023/ejercicio2.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main ()
+{
+	float c[5];
+	float r=0;
+	printf ("DEPURAR EL SIGUIENTE PROGRAMA\n");
+	for (int i=0;i<5;i++){
+		printf ("%d\n",i+1);
+		scanf ("%f",&c[5]);
+		printf ("\n");
+		printf ("%.2f\n",r/5);
+	}
+	return 0;
+}
+
+
+
