7bafb20fda5ac5404d840dc9b5e4bd0977a6e752
HilArabelle <132083357+HilArabelle@users.noreply.github.com>
2023-06-14 12:56:06 -0600

    Add files via upload

---
+++ 14.06.2023-2/Ejercicio2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int func (void){
+	puts("Primera funcion");
+	return 0;
+}
+void func1(void){
+	puts("Segunda funcion");
+	return;
+}
+int func2(){
+	puts("Tercera funcion");
+	return 0;
+}
+{
+	func();
+	func1();
+	func2();
+	return 0;
+}
+
