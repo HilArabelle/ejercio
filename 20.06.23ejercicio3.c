5c6db723b4705fdedba5b3c0550f10d92033bbe3
HilArabelle <132083357+HilArabelle@users.noreply.github.com>
2023-06-20 10:02:10 -0600

    Add files via upload

---
+++ 20.06.23ejercicio3.c
@@ -0,0 +1,23 @@
+#include <ctype.h>
+
+int main()
+{
+	char resp; //respuesta del usuario 
+	char c;
+	printf("¿es masculino o femenino (M/F)?");
+	scanf("%c",&resp);
+	resp= toupper(resp);
+	switch(resp)
+	{
+		case 'M':
+			puts ("es un enfermero");
+			break;
+		case 'F':
+			puts("es una enfermera ");
+			break;
+			default:
+				puts("no es enfermero ni enfermera");
+				break;
+	}
+	return 0;
+}
